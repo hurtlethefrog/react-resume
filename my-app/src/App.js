88e66b70c511d1484bcbc50f0062c886a57ee544
Duncan H <duncan.haran@gmail.com>
2019-10-26 08:41:55 -0400

    empty userionfo componenet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import './App.css';
 import Resume from "./data/me.json";
 
 function App() {
---
+++ my-app/src/components/Userinfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.scss";
+
+export default function Userinfo(props) {
+  return (
+      <section className="username">
+        <button>
+        </button>
+      </section>
+  );
+}
