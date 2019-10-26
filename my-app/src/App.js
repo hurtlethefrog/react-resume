d105a09f51123d89509281f790e6e6d86d5204d1
Duncan H <duncan.haran@gmail.com>
2019-10-26 08:48:53 -0400

    pass down userinfo in props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Resume from "./data/me.json";
+import resume from "./data/me.json";
+import Userinfo from "./components/Userinfo"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>{Resume.firstName}</div>
+      <Userinfo name={resume.name} email={resume.email} phoneNumber={resume.phoneNumber} location={resume.location} linkedin={resume.linkedin} github={resume.github} bio={resume.bio}/>
       </header>
     </div>
   );
--- my-app/src/components/Userinfo.js
+++ my-app/src/components/Userinfo.js
@@ -6,6 +6,7 @@ export default function Userinfo(props) {
   return (
       <section className="username">
         <button>
+            {props.name}
         </button>
       </section>
   );
