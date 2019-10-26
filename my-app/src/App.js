d9eff6ad6584868e74f6460c1824027730abada8
Duncan H <duncan.haran@gmail.com>
2019-10-26 10:27:37 -0400

    empty education/projects/emplyment components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import resume from "./data/me.json";
+import "./styles.scss";
 import Userinfo from "./components/Userinfo";
 import Skills from "./components/Skills";
-import "./styles.scss";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Employment from "./components/Employment";
 
 function App() {
-  console.log(resume.skills)
+  console.log(resume.skills);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +21,15 @@ function App() {
           github={resume.github}
           bio={resume.bio}
         />
-        <Skills languages={resume.skills.languages} frameworks={resume.skills.frameworks} libraries={resume.skills.libraries} testingTools={resume.skills.testingTools}/>
+        <Skills
+          languages={resume.skills.languages}
+          frameworks={resume.skills.frameworks}
+          libraries={resume.skills.libraries}
+          testingTools={resume.skills.testingTools}
+        />
+        <Projects projects={resume.projects} />
+        <Education education={resume.education} />
+        <Employment employment={resume.employment} />
       </header>
     </div>
   );
---
+++ my-app/src/components/Education.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.scss";
+
+export default function Education(props) {
+  return <section className="education">
+      
+  </section>;
+}
