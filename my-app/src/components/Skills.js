509d6458d42486a1b0a19b52b9a1a86bbdfb57c3
Duncan H <duncan.haran@gmail.com>
2019-10-28 08:47:41 -0400

    list creator helper for skills

--- my-app/src/components/Skills.js
+++ my-app/src/components/Skills.js
@@ -3,9 +3,18 @@ import React from "react";
 import "./styles.scss";
 
 export default function Skills(props) {
+  const listCreator = arr => {
+    return arr.map(item => {
+      return <li key={item}>{item}</li>;
+    });
+  };
+
   return (
     <section className="skills">
-      <div>{props.languages}</div>
+      <ul>{listCreator(props.languages)}</ul>
+      <ul>{listCreator(props.frameworks)}</ul>
+      <ul>{listCreator(props.libraries)}</ul>
+      <ul>{listCreator(props.testingTools)}</ul>
     </section>
   );
 }
