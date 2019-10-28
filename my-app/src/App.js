5d6353fabe5fbe4d9f950915651e9a14568baa94
Duncan H <duncan.haran@gmail.com>
2019-10-28 07:54:33 -0400

    react router and nav added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import resume from "./data/me.json";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./styles.scss";
 import Userinfo from "./components/Userinfo";
 import Skills from "./components/Skills";
@@ -9,29 +10,61 @@ import Employment from "./components/Employment";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/employment">Employment</Link>
+            </li>
+            <li>
+              <Link to="/education">Education</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/skills">
+            <Skills
+              languages={resume.skills.languages}
+              frameworks={resume.skills.frameworks}
+              libraries={resume.skills.libraries}
+              testingTools={resume.skills.testingTools}
+            />
+          </Route>
+          <Route path="/projects">
+            <Projects projects={resume.projects} />
+          </Route>
+          <Route path="/education">
+            <Education education={resume.education} />
+          </Route>
+          <Route path="/employment">
+            <Employment employment={resume.employment} />
+          </Route>
+          <Route path="/">
+            <Userinfo
+              name={resume.name}
+              email={resume.email}
+              phoneNumber={resume.phoneNumber}
+              location={resume.location}
+              linkedin={resume.linkedin}
+              github={resume.github}
+              bio={resume.bio}
+            />
+          </Route>
+        </Switch>
+
         {/* use react router to display userinfo on homepage, with option to change to other sections, take whole page to display */}
-        <Userinfo
-          name={resume.name}
-          email={resume.email}
-          phoneNumber={resume.phoneNumber}
-          location={resume.location}
-          linkedin={resume.linkedin}
-          github={resume.github}
-          bio={resume.bio}
-        />
-        <Skills
-          languages={resume.skills.languages}
-          frameworks={resume.skills.frameworks}
-          libraries={resume.skills.libraries}
-          testingTools={resume.skills.testingTools}
-        />
-        <Projects projects={resume.projects} />
-        <Education education={resume.education} />
-        <Employment employment={resume.employment} />
-      </header>
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- my-app/src/components/Skills.js
+++ my-app/src/components/Skills.js
@@ -5,7 +5,7 @@ import "./styles.scss";
 export default function Skills(props) {
   return (
     <section className="skills">
-      
+      <div>{props.languages}</div>
     </section>
   );
 }
